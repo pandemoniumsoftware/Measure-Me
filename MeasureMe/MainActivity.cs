651eb8194a073f68ab7ae4e0f64b65b0f11510e3
James Armes <jamesleearmes@gmail.com>
2016-07-05 21:36:55 -0600

    Implemented Tab Nav
    Implemented tab navigation (PagerTabStrip) - will likely update with a
    standard tab layout.

--- MeasureMe/MainActivity.cs
+++ MeasureMe/MainActivity.cs
@@ -5,9 +5,8 @@ using Android.OS;
 namespace MeasureMe
 {
 	[Activity(Label = "Measure Me", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
+	public class MainActivity : Android.Support.V4.App.FragmentActivity
 	{
-		int count = 1;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -16,11 +15,15 @@ namespace MeasureMe
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+			var fragments = new Android.Support.V4.App.Fragment[] {
+				new FavoritesFragment(),
+				new LogFragment(),
+				new UserInformationFragment()
+			};
 
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+			var viewPager = FindViewById<Android.Support.V4.View.ViewPager>(Resource.Id.viewPager);
+
+			viewPager.Adapter = new ViewPagerFragmentAdapter(base.SupportFragmentManager, fragments);
 		}
 	}
 }
